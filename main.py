40ce8096445543bade69df247c35efd74bbe6b43
Alessandro Maggio <alessandro@vps521890.ovh.net>
2018-05-19 11:50:55 +0200

    Finally fix with new istance of list user members

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ if not os.path.exists("logs"):
     os.makedirs("logs")
 
 def welcome(bot, update):
-    if(update.message.new_chat_member):
+    for new_user_obj in update.message.new_chat_members:
         open("logs/logs_" + time.strftime('%d_%m_%Y') + ".txt","w").write("\nupdate status: " + str(update))
-    	chat_id = update.message.chat.id
+        chat_id = update.message.chat.id
         new_user = ""
         message_rnd = random.choice(message_list)
         WELCOME_MESSAGE = open('message/' + message_rnd , 'r').read().replace("\n", "")
 
-        if(update.message.new_chat_member.username != ""):
-            new_user = "@" + update.message.new_chat_member.username;
-        else:
-            new_user = update.message.new_chat_member.first_name;
+        try:
+            new_user = "@" + new_user_obj['username']
+        except Exception as e:
+            new_user = new_user_obj['first_name'];
 
-    	bot.sendMessage(chat_id=chat_id, text=WELCOME_MESSAGE.replace("{{username}}",str(new_user)), parse_mode='HTML')
+        bot.sendMessage(chat_id=chat_id, text=WELCOME_MESSAGE.replace("{{username}}",str(new_user)), parse_mode='HTML')
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
@@ -36,7 +36,7 @@ def error(bot, update, error):
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.status_update, welcome))
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, welcome))
     dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
